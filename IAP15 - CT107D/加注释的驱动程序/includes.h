d372fabf4366c70f029f1b29004f3a21a2a76a63
Swordmaster <15565132894@163.com>
2018-03-27 22:36:46 +0800

    修改驱动

---
+++ IAP15 - CT107D/加注释的驱动程序/includes.h
@@ -0,0 +1,17 @@
+#ifndef __INCLUDES_H__
+#define __INCLUDES_H__
+
+#define uint unsigned int
+#define uchar unsigned char
+
+#include"STC15F2K60S2.h"
+#include"allinit.h"
+#include"smg.h"
+#include"keypad.h"
+#include"ds1302.h"
+//#include"iic.h"
+//#include"onewire.h"
+//#include"PWM.h"
+#include"usart.h"
+
+#endif 
